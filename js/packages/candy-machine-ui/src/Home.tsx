de353eeb2d7b76eb05754fadd11bcbdf8af369cb
emberist <13602797+emberist@users.noreply.github.com>
2022-03-01 19:44:15 +0100

    fix: logo height

--- js/packages/candy-machine-ui/src/Home.tsx
+++ js/packages/candy-machine-ui/src/Home.tsx
@@ -290,6 +290,11 @@ const Home = (props: HomeProps) => {
             borderRadius: 6,
           }}
         >
+          <img
+            alt="staratlasitalia logo"
+            src="/logo.png"
+            style={{ marginBottom: 20, height: '80px' }}
+          />
           {!wallet.connected ? (
             <ConnectButton>Connect Wallet</ConnectButton>
           ) : (
@@ -297,104 +302,92 @@ const Home = (props: HomeProps) => {
               {candyMachine && (
                 <Grid
                   container
-                  direction="column"
+                  direction="row"
                   justifyContent="center"
                   wrap="nowrap"
                 >
-                  <img
-                    alt="staratlasitalia logo"
-                    src="/logo.png"
-                    style={{ marginBottom: 20 }}
-                  />
-                  <Grid
-                    container
-                    direction="row"
-                    justifyContent="center"
-                    wrap="nowrap"
-                  >
-                    <Grid item xs={3}>
-                      <Typography variant="body2" color="textSecondary">
-                        Remaining
-                      </Typography>
-                      <Typography
-                        variant="h6"
-                        color="textPrimary"
-                        style={{
-                          fontWeight: 'bold',
-                        }}
-                      >
-                        {`${itemsRemaining}`}
-                      </Typography>
-                    </Grid>
-                    <Grid item xs={4}>
-                      <Typography variant="body2" color="textSecondary">
-                        {isWhitelistUser && discountPrice
-                          ? 'Discount Price'
-                          : 'Price'}
-                      </Typography>
-                      <Typography
-                        variant="h6"
-                        color="textPrimary"
-                        style={{ fontWeight: 'bold' }}
-                      >
-                        {isWhitelistUser && discountPrice
-                          ? `◎ ${formatNumber.asNumber(discountPrice)}`
-                          : `◎ ${formatNumber.asNumber(
-                              candyMachine.state.price,
-                            )}`}
-                      </Typography>
-                    </Grid>
-                    <Grid item xs={5}>
-                      {isActive && endDate && Date.now() < endDate.getTime() ? (
-                        <>
-                          <MintCountdown
-                            key="endSettings"
-                            date={getCountdownDate(candyMachine)}
-                            style={{ justifyContent: 'flex-end' }}
-                            status="COMPLETED"
-                            onComplete={toggleMintButton}
-                          />
-                          <Typography
-                            variant="caption"
-                            align="center"
-                            display="block"
-                            style={{ fontWeight: 'bold' }}
-                          >
-                            TO END OF MINT
-                          </Typography>
-                        </>
-                      ) : (
-                        <>
-                          <MintCountdown
-                            key="goLive"
-                            date={getCountdownDate(candyMachine)}
-                            style={{ justifyContent: 'flex-end' }}
-                            status={
-                              candyMachine?.state?.isSoldOut ||
-                              (endDate && Date.now() > endDate.getTime())
-                                ? 'COMPLETED'
-                                : isPresale
-                                ? 'PRESALE'
-                                : 'LIVE'
-                            }
-                            onComplete={toggleMintButton}
-                          />
-                          {isPresale &&
-                            candyMachine.state.goLiveDate &&
-                            candyMachine.state.goLiveDate.toNumber() >
-                              new Date().getTime() / 1000 && (
-                              <Typography
-                                variant="caption"
-                                align="center"
-                                display="block"
-                                style={{ fontWeight: 'bold' }}
-                              >
-                                UNTIL PUBLIC MINT
-                              </Typography>
-                            )}
-                        </>
-                      )}
-                    </Grid>
+                  <Grid item xs={3}>
+                    <Typography variant="body2" color="textSecondary">
+                      Remaining
+                    </Typography>
+                    <Typography
+                      variant="h6"
+                      color="textPrimary"
+                      style={{
+                        fontWeight: 'bold',
+                      }}
+                    >
+                      {`${itemsRemaining}`}
+                    </Typography>
+                  </Grid>
+                  <Grid item xs={4}>
+                    <Typography variant="body2" color="textSecondary">
+                      {isWhitelistUser && discountPrice
+                        ? 'Discount Price'
+                        : 'Price'}
+                    </Typography>
+                    <Typography
+                      variant="h6"
+                      color="textPrimary"
+                      style={{ fontWeight: 'bold' }}
+                    >
+                      {isWhitelistUser && discountPrice
+                        ? `◎ ${formatNumber.asNumber(discountPrice)}`
+                        : `◎ ${formatNumber.asNumber(
+                            candyMachine.state.price,
+                          )}`}
+                    </Typography>
+                  </Grid>
+                  <Grid item xs={5}>
+                    {isActive && endDate && Date.now() < endDate.getTime() ? (
+                      <>
+                        <MintCountdown
+                          key="endSettings"
+                          date={getCountdownDate(candyMachine)}
+                          style={{ justifyContent: 'flex-end' }}
+                          status="COMPLETED"
+                          onComplete={toggleMintButton}
+                        />
+                        <Typography
+                          variant="caption"
+                          align="center"
+                          display="block"
+                          style={{ fontWeight: 'bold' }}
+                        >
+                          TO END OF MINT
+                        </Typography>
+                      </>
+                    ) : (
+                      <>
+                        <MintCountdown
+                          key="goLive"
+                          date={getCountdownDate(candyMachine)}
+                          style={{ justifyContent: 'flex-end' }}
+                          status={
+                            candyMachine?.state?.isSoldOut ||
+                            (endDate && Date.now() > endDate.getTime())
+                              ? 'COMPLETED'
+                              : isPresale
+                              ? 'PRESALE'
+                              : 'LIVE'
+                          }
+                          onComplete={toggleMintButton}
+                        />
+                        {isPresale &&
+                          candyMachine.state.goLiveDate &&
+                          candyMachine.state.goLiveDate.toNumber() >
+                            new Date().getTime() / 1000 && (
+                            <Typography
+                              variant="caption"
+                              align="center"
+                              display="block"
+                              style={{ fontWeight: 'bold' }}
+                            >
+                              UNTIL PUBLIC MINT
+                            </Typography>
+                          )}
+                      </>
+                    )}
                   </Grid>
                 </Grid>
               )}
